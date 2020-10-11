1c0f386db5e6713527329930823879350972f779
Noah Davis <noahdavis319@gmail.com>
2020-10-11 06:41:13 -0700

    Removed PyBuilder Usage

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,18 +12,12 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('.'))
-sys.path.append(os.path.abspath('../main/python/'))
+sys.path.insert(0, os.path.abspath('../../src/main/python/'))
 
-# Show Python's spaceial function in autodoc (e.g. __init__)
-autodoc_default_flags = ['members', 'show-inheritance']
-
-# group members by visibility
-autodoc_member_order = 'bysource'
 
 # -- Project information -----------------------------------------------------
 
-project = 'SSMS'
+project = 'Ship Strain Measurement System'
 copyright = '2020, Noah Davis'
 author = 'Noah Davis'
 
@@ -33,25 +27,20 @@ author = 'Noah Davis'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+import sphinx_rtd_theme
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-source_suffix = '.rst'
-master_doc = 'index'
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -59,17 +48,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-
-# -- Extension configuration -------------------------------------------------
-
-# -- Options for todo extension ----------------------------------------------
-
-# If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
